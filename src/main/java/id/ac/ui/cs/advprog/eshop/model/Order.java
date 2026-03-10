7fd7ed257ec9e547ea676fab80a6876d775ccb6f
AsukaOurijya <azkaawliya06@gmail.com>
2026-03-10 11:50:58 +0700

    [REFACTOR] Apply OrderStatus enum check to Order model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,20 +1,13 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import lombok.Builder;
 import lombok.Getter;
 import java.util.List;
-import java.util.Set;
 
 @Builder
 @Getter
 public class Order {
-    private static final Set<String> VALID_STATUSES = Set.of(
-            "WAITING_PAYMENT",
-            "FAILED",
-            "SUCCESS",
-            "CANCELLED"
-    );
-
     private final String id;
     private final List<Product> products;
     private final Long orderTime;
@@ -24,21 +17,28 @@ public class Order {
     public Order(String id, List<Product> products, Long orderTime, String author) {
         validateProducts(products);
         this.id = id;
-        this.products = products;
         this.orderTime = orderTime;
         this.author = author;
-        this.status = "WAITING_PAYMENT";
+        this.status = OrderStatus.WAITING_PAYMENT.getValue();
+
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.products = products;
+        }
     }
 
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
         this(id, products, orderTime, author);
-
         this.setStatus(status);
     }
 
     public void setStatus(String status) {
-        validateStatus(status);
-        this.status = status;
+        if (OrderStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validateProducts(List<Product> products) {
@@ -48,7 +48,7 @@ public class Order {
     }
 
     private void validateStatus(String status) {
-        if (status == null || !VALID_STATUSES.contains(status)) {
+        if (!OrderStatus.contains(status)) {
             throw new IllegalArgumentException("Invalid status");
         }
     }
